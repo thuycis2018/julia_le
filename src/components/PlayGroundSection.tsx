e9918dfaa9acde799a807bbd047e0da6b9ae29ca
TL <thuycis@gmail.com>
2024-09-16 21:37:15 -0700

    refactor(ci): try deployment via Dockerfile

--- src/components/PlayGroundSection.tsx
+++ src/components/PlayGroundSection.tsx
@@ -6,8 +6,7 @@ export default function PlayGroundSection() {
       <div>      
         <ul className="text-left">
           <li className="p-5"><FontAwesomeIcon icon={faLaptopCode} className="mr-2" /><a href="https://j-le.pages.dev/" title="https://j-le.pages.dev/">https://j-le.pages.dev/</a> is built with TypeScript, GraphQL, Tailwind CSS, Vite, and Redux. Deployment is managed using Cloudflare, which deploys changes automatically whenever there is a push to the main branch in the GitHub repository.</li>
-          <li className="p-5"><FontAwesomeIcon icon={faLaptopCode} className="mr-2" /><a href="https://nextjs-openai-chi.vercel.app/" title="Next.JS app" target="_blank">https://nextjs-openai-chi.vercel.app/</a> is built with Next.js, TypeScript, GraphQL, and Tailwind CSS. Deployment is handled by Vercel, which automatically deploys changes whenever there is a push to the main branch in the GitHub repository.</li>
-          <li className="p-5"><FontAwesomeIcon icon={faLaptopCode} className="mr-2" /><a href="https://graphql-github.pages.dev/" target="_blank" title="Open Cloudflare page">https://graphql-github.pages.dev/</a> is built with React and Apollo Client for the frontend, and a Node.js Express server to relay requests and responses between the frontend and GitHub GraphQL endpoints. Deployment is managed using Cloudflare, which deploys changes automatically whenever there is a push to the main branch in the GitHub repository.</li>
+          <li className="p-5"><FontAwesomeIcon icon={faLaptopCode} className="mr-2" /><a href="https://julia-le.vercel.app/" title="Next.JS app" target="_blank">https://julia-le.vercel.app/</a> is built with Next.js, TypeScript, GraphQL, and Tailwind CSS. Deployment is handled by Vercel, which automatically deploys changes whenever there is a push to the main branch in the GitHub repository.</li>
         </ul>
       </div>
     );
