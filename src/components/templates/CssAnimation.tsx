573e17b60507c7c22b08334d7fb9f6fb81330365
TL <thuycis@gmail.com>
2024-10-07 14:26:26 -0700

    refactor(style): use specific css classes

--- src/components/templates/CssAnimation.tsx
+++ src/components/templates/CssAnimation.tsx
@@ -4,15 +4,15 @@ import './CssAnimation.css';
 const CssAnimation: React.FC = () => {
   return (
     <>
-      <main>
-          <section>
-              <h1>
-                  <span>Design</span>
-                  <span>&</span>
-                  <span>Price</span>
+      <main className="lp-main">
+          <section className="lp-section">
+              <h1 className="lp-h1">
+                  <span className="lp-span">Design</span>
+                  <span className="lp-span">&</span>
+                  <span className="lp-span">Price</span>
               </h1>
-              <p>Create Your Unique Furniture</p>
-              <button className="design">Design Now</button>
+              <p className="lp-p">Create Your Unique Furniture</p>
+              <button className="lp-button">Design Now</button>
           </section>
       </main>
     </>
