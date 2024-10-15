dd37fcb44b7adb608b23bab36bc1c56547c5f66a
TL <thuycis@gmail.com>
2024-10-15 16:53:43 -0700

    refactor(style): rename some files

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,6 +1,6 @@
 export default function HeroBanner() {
     return (
-        <div className="bg-[url('/images/computer4.jpg')] md:bg-[url('/images/teambg_sm.jpg')] bg-no-repeat bg-left bg-cover h-80 shadow-[0_1px_8px_#223055]">
+        <div className="bg-[url('/images/hero_banner_mobile.jpg')] md:bg-[url('/images/hero_banner.jpg')] bg-no-repeat bg-left bg-cover h-80 shadow-[0_1px_8px_#223055]">
         </div> 
     );
   }
--- src/components/StackedImages.tsx
+++ src/components/StackedImages.tsx
@@ -3,23 +3,23 @@ export default function StackedImages() {
         <div className= "container mx-auto mb-10 p-4">
             <ul className="teamMembers list-none grid grid-cols-10 items-center mx-auto">
                 <li className="row-start-1 row-end-2 col-start-1 col-end-4 order-1">
-                <img src="/images/team_s.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill - Project Management" />
+                <img src="/images/stacked_image1.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill - Project Management" />
                 </li>
                 
                 <li className="row-start-1 row-end-2 col-start-3 col-end-6">
-                <img src="/images/computer3_s.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill -  Web Development" />
+                <img src="/images/stacked_image2.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill -  Web Development" />
                 </li>
 
                 <li className="row-start-1 row-end-2 col-start-4 col-end-8 order-1 text-center">
-                <img src="/images/computer2_s.jpg" className="rounded-full w-[70%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Value -  Work Hard" />
+                <img src="/images/stacked_image3.jpg" className="rounded-full w-[70%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Value -  Work Hard" />
                 </li>
 
                 <li className="row-start-1 row-end-2 col-start-[-3] col-end-6 text-right">
-                <img src="/images/team2_s.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Value - Team Work" />
+                <img src="/images/stacked_image4.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Value - Team Work" />
                 </li>
 
                 <li className="row-start-1 row-end-2 col-start-[-4] col-end-[-1] text-right">
-                <img src="/images/computer_s.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill - System Integrations" />
+                <img src="/images/stacked_image5.jpg" className="rounded-full w-[80%] shadow-[1px_1px_3px_#fff,_2px_2px_6px_rgba(28,121,138,0.5)] border-2 border-[rgba(255,255,255,0.8)]" alt="Skill - System Integrations" />
                 </li>
             </ul>
         </div>
