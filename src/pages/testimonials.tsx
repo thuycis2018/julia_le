f4eee0b689c9002d093b8a906bccaa3353049eaf
TL <thuycis@gmail.com>
2024-10-15 19:55:52 -0700

    fix(image): update image path

--- src/pages/testimonials.tsx
+++ src/pages/testimonials.tsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 export default function TestimonialPage() {
   return (
     <div>
-     <div className="bg-[url('/images/computer1.jpg')] bg-no-repeat bg-left bg-cover h-40 opacity-70">      
+     <div className="bg-[url('/images/hero_testimonial.jpg')] bg-no-repeat bg-left bg-cover h-40 opacity-70">      
         
       </div>
       <h2 className="text-black text-3xl font-bold text-center p-4">Testimonials</h2>
